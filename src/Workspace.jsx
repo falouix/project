e10ed554386c120567ccc662ce591fc66bd65e84
falouix <falouix@gmail.com>
2022-11-19 11:35:41 +0100

    close app

--- src/Workspace.jsx
+++ src/Workspace.jsx
@@ -1,6 +1,7 @@
 
 import './App.css';
-import './apps.css'
+import './apps.css';
+import React, { useState } from 'react';
 import GridLayout from "react-grid-layout";
 import {
     Card,
@@ -10,34 +11,74 @@ import {
     Typography,
   } from "@material-tailwind/react";
 
-  
-const layout = [
+
+
+function Workspace() {
+   
+  const [somethingHere, setSomethingHere] = useState();
+  function closeApp(){
+    setSomethingHere();
+  }
+  function renderApp(id){
+   setSomethingHere(
+    <Card className='app_container'>
+           <div className="app_container_topBar">
+            <h3>{id}</h3>
+           <svg 
+            className="close_window w-6 h-6" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" viewBox="0 0 24 24" 
+            strokeWidth={1.5} stroke="currentColor"
+            onClick={closeApp} 
+            >
+
+               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+           </svg>
+ 
+           </div>
+             <h1 class="something">{id}</h1>
+     </Card>
+   )
+ }
+ const layout = [
   { i: "app1", x: 0, y: 0, w: 1, h: 1,openApp:(e,id)=>{
     e.preventDefault()
+    renderApp(id);
     console.log('app to open : ',id)
 } },
   { i: "app2", x: 1, y: 0, w: 1, h: 1,openApp:(e,id)=>{
     e.preventDefault()
+    renderApp(id);
     console.log('app to open : ',id)
 } },
   { i: "app3", x: 2, y: 0, w: 1, h: 1,openApp:(e,id)=>{
     e.preventDefault()
+    renderApp(id);
     console.log('app to open : ',id)
 } },
   { i: "app4", x: 3, y: 0, w: 1, h: 1,openApp:(e,id)=>{
     e.preventDefault()
+    renderApp(id);
     console.log('app to open : ',id)
 } },
   { i: "app5", x: 4, y: 0, w: 1, h: 1,openApp:(e,id)=>{
     e.preventDefault()
+    renderApp(id
+      
+      );
     console.log('app to open : ',id)
 } }
 ];
 const elements = layout.map(item=>{
+  
     return(
         
-     <Card key={item.i} className="w-96" onClick={event=>{item.openApp(event,item.i)}}>
-        <CardHeader color="blue" className="relative h-56">{item.i}</CardHeader>
+     <Card 
+      key={item.i} 
+      className="w-96 app_card" 
+      onClick={event=>{item.openApp(event,item.i)}}
+      >
+        <Typography variant="h3" className="card_title">{item.i}</Typography>
      </Card>
     )
  })
@@ -45,21 +86,24 @@ const elements = layout.map(item=>{
 
 
 
-function Workspace() {
-   
+
+
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <GridLayout layout={layout} cols={5} rowHeight={300} width={1000}>
+        <GridLayout 
+        layout={layout} 
+        cols={5} 
+        rowHeight={300} 
+        width={1000}
+        containerWidth={300}
+        >
            {
             elements
            }
           </GridLayout>
           
-        <div id="app_container">
-
+        <div>
+            {somethingHere}
         </div>
       </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      sm: "640px",
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
   },
   plugins: [],
 });
